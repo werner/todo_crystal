7d1861924acec2d41a36457cced6555213b821dd
Werner <werner_a_e@yahoo.es>
2015-04-16 21:20:41 -0430

    add index page

--- app/main.cr
+++ app/main.cr
@@ -1,7 +1,29 @@
+require "html/builder"
 require "sqlite3"
 require "../web_server/server.cr"
 
 app = Server::Base.new
+
+app.get "/" do
+  db_filename = "db/data.sqlite3"
+  db = SQLite3::Database.new( db_filename ) 
+  str = HTML::Builder.new.build do
+    html do
+      head do
+        title { text "Todo App" }
+      end
+      body do
+        ul do
+          db.query("select * from items") do |row|
+            li { row["description"] }
+          end
+        end
+      end
+    end
+  end
+  db.close
+  str
+end
 
 PORT = 1234
 p "Server running on #{PORT}"
