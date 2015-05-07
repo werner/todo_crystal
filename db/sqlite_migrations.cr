363ddb907b1b2354a5c4eded4b41832966b88875
Werner <werner_a_e@yahoo.es>
2015-05-07 07:46:19 -0430

    rename migrations by sqlite_migrations

---
+++ db/sqlite_migrations.cr
@@ -0,0 +1,14 @@
+require "sqlite3"
+def create_table(db, table_name)
+  if db.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='#{table_name}';").empty?
+    yield db
+  end
+end
+
+
+db_filename = "db/data.sqlite3"
+db = SQLite3::Database.new( db_filename ) 
+create_table(db, "tasks") do |db|
+  db.execute("create table tasks(id integer primary key not null, description text not null, done boolean)")
+end
+db.close
