22fd98e74f36ed6961c8f96881faaef0556dfedc
Werner <werner_a_e@yahoo.es>
2015-09-02 11:22:12 -0430

    remove code not needed from sessions

--- src/models/session.cr
+++ src/models/session.cr
@@ -1,10 +1,4 @@
 require "crypto/bcrypt/base64"
 
 class Session < Amatista::Model
-  def self.create(email, raw_password)
-    password = Crypto::Bcrypt.new.digest(raw_password)
-    result = DB.exec({Int32, String}, 
-                     "select id, email from users where email = $1 and password = $2",
-                    [email, password])
-  end
 end
